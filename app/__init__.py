323f6eab29ad282128a9dc4b34ef4e5d218d0106
pengyi <535719197@qq.com>
2018-11-22 09:23:04 +0800

    修改项目结构,增加migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.home import home as home_blueprint
 from app.admin import admin as admin_blueprint
@@ -6,3 +6,7 @@ from app.admin import admin as admin_blueprint
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+
+
+app.register_blueprint(home_blueprint)
+app.register_blueprint(admin_blueprint, url_prefix="/admin")
